d672129e0f887a425943339ecc0d28cc9b689372
iyermeenakshi99 <iyermeenakshi99@gmail.com>
2020-10-17 10:34:11 +0530

    This is my second commit of Clone

--- src/test/java/GitTest1.java
+++ src/test/java/GitTest1.java
@@ -13,6 +13,11 @@ public class GitTest1 {
 	}
 	
 	@Test
+	public void method3() {
+		System.out.println("This is method 3");
+	}
+	
+	@Test
 	public void method4() {
 		System.out.println("This is method 4");
 	}
